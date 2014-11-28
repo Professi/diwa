6ca765797adf384efeded00e7c9d0861ecf34c2d
Christian Ehringfeld <c.ehringfeld@t-online.de>
2014-11-28 19:53:50 +0100

    .

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=diwa',
     'username' => 'root',
-    'password' => '',
+    'password' => 'test',
     'charset' => 'utf8',
 ];
--- models/User.php
+++ models/User.php
@@ -1,5 +1,19 @@
 <?php
-
+/* Copyright (C) 2014  Christian Ehringfeld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace app\models;
 
 class User extends \yii\base\Object implements \yii\web\IdentityInterface
@@ -7,16 +21,12 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
     public $id;
     public $username;
     public $password;
-    public $authKey;
-    public $accessToken;
 
     private static $users = [
         '100' => [
             'id' => '100',
             'username' => 'admin',
             'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
         ],
         '101' => [
             'id' => '101',
